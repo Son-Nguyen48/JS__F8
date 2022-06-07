c5246c390fd2e54a15e18c8a0d6098ba512a1f3b
Son-Nguyen48 <sonnt.B16CN303@stu.ptit.edu.vn>
2022-06-07 09:18:45 +0700

    new day

--- main.js
+++ main.js
@@ -432,5 +432,41 @@ var boxNode = document.querySelector(".box-1");
 //Do some stuff here
 //Then work with all li child of box-1 div
 console.log(boxNode);
+boxNode.style = "color: red;";
+boxNode.classList.add("box-shadow");
 var liNodeList1 = boxNode.querySelectorAll("li");
 console.log(liNodeList1);
+console.log("----------------------------------------");
+const div = document.createElement("div");
+div.className = "foo";
+
+// our starting state: <div class="foo"></div>
+console.log(div.outerHTML);
+
+// use the classList API to remove and add classes
+div.classList.remove("foo");
+div.classList.add("anotherclass");
+
+// <div class="anotherclass"></div>
+console.log(div.outerHTML);
+
+// if visible is set remove it, otherwise add it
+div.classList.toggle("visible");
+let i = 0;
+// add/remove visible, depending on test conditional, i less than 10
+div.classList.toggle("visible", i < 10);
+
+console.log(div.classList.contains("foo"));
+
+// add or remove multiple classes
+div.classList.add("foo", "bar", "baz");
+div.classList.remove("foo", "bar", "baz");
+
+// add or remove multiple classes using spread syntax
+const cls = ["foo", "bar"];
+div.classList.add(...cls);
+console.log(div.classList);
+div.classList.remove(...cls);
+
+// replace class "foo" with class "bar"
+div.classList.replace("foo", "bar");
